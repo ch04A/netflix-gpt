bb4ae6632eaeda349ba51f1c703f6ee21da84cd9
ch04A <aasritha.chunduru@gmail.com>
2025-01-12 13:13:45 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Header from "./Components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    </div>
+  );
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <img src="" />
+      Header
+    </div>
+  );
+};
+
+export default Header;
